db07805b1fc9fa5fd663823803ccdb11df672cd5
AswathiMohan23 <achu999here@gmail.com>
2023-01-11 17:46:36 +0530

    [ASWATHI] added : code to find daily employee wage

--- EmpWage/EmployeeWageCalculator.py
+++ EmpWage/EmployeeWageCalculator.py
@@ -1,11 +1,24 @@
 import random
 
-print("Welcome to employee wage calculator")
 
-# ------------------------------------------------ UC1 ------------------------------------------------------
-random_value = random.randint(0, 1)
-if random_value == 1:
-    print(random_value, "====> Employee is present")
+def random_check():
+    random_value = random.randint(0, 1)
+    if random_value == 1:
+        print(random_value, "====> Employee is present")
+        return 1
+    else:
+        print(random_value, "====> Employee is absent")
+        return 0
+
+
+print("Welcome to employee wage calculator")
+wage_per_hour = 20
+full_day_hour = 8
+if random_check() == 1:
+    dailyWage = 20 * 8
+    print("Daily wage = ", dailyWage)
 else:
-    print(random_value, "====> Employee is absent")
+    print("Daily wage = 0 ")
+
+
 
